cb2b6976e7dd93e42292c532bd0b335a9b3808af
siyabonga <siyabongasamkelociam@gmail.com>
2025-12-04 08:17:47 +0200

    first commit cleaning up all the useless react files saving the figma design files of the design folder installing dependencies

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
