7757b2ba98792f6bf6658464d57bbce5e14ea450
siyabonga <siyabongasamkelociam@gmail.com>
2025-12-05 05:27:01 +0200

    added browser router and styled components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,47 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ThemeProvider } from "styled-components";
+
 function App() {
-  return <div className="App"></div>;
+  const theme = {
+    color: {
+      primary: "#FF8F00",
+      secondary: "#DFDFDF",
+      tetiary: "rgba(0,0,0,0.7)",
+      text: "rgba(0,0,0,0.7)",
+      smallText: "rgba(0, 0, 0, 0.6)",
+    },
+    size: {
+      small: "13px",
+      medium: "17px",
+      large: "22px",
+      extraLarge: "32px",
+    },
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route index element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/*" element={<HomePage />} />
+      </Route>
+    )
+  );
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
