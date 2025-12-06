4cd6c596e656d490bc03e562f3e23043f3432a04
siyabonga <siyabongasamkelociam@gmail.com>
2025-12-06 02:18:19 +0200

    added part of the hero section

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,33 @@
 import { DecorationsContainer, HomeContainer } from "./Home.styled";
 import BackgroundImage from "../assets/img/background.jpeg";
+import Header from "./common/Header";
+import { BigText, SmallText } from "./Texts.styled";
+import { Button } from "./common/Button.styled";
 const Home = () => {
   return (
     <HomeContainer image={BackgroundImage}>
       <DecorationsContainer>
+        {/* because of the complexity of the interface i used position : absolute on the header */}
+        {/* to make it float above the home screen */}
+        <Header />
         <div className="div1">
           <div className="logo"></div>
           <div className="socialmedial-icons"></div>
         </div>
         <div className="div2">
-          <div className="hero-section"></div>
+          <div className="hero-section">
+            <BigText>Mandeni Municipality</BigText>
+            <div>
+              <SmallText>
+                Mandeni is a community built on strength, resilience, and unity.
+                Every day, our people rise with purpose — working hard,
+                supporting one another, and shaping a future filled with
+                opportunity. From our townships to our factories and farmlands,
+                Mandeni stands as a place of courage, dignity, and hope.
+              </SmallText>
+            </div>
+            <Button>Contact Us</Button>
+          </div>
           <div className="ourteam"></div>
         </div>
         <div className="div3">
