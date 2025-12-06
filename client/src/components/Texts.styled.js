2a66f8c9cd90a6694b1f4b79b8a910e8e594ab70
siyabonga <siyabongasamkelociam@gmail.com>
2025-12-06 02:21:21 +0200

    text for all the website

---
+++ client/src/components/Texts.styled.js
@@ -0,0 +1,30 @@
+//This is where i will design and set all my texts in the app
+import styled from "styled-components";
+
+export const BigText = styled.h1`
+  font-family: "Kablammo";
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: normal;
+  /* font-variation-settings: "MORF" 0; */
+  font-size: 3.8rem;
+  color: white;
+`;
+
+export const SmallText = styled.p`
+  font-family: "League Spartan";
+  font-weight: normal;
+  font-style: normal;
+  font-size: 18px;
+  color: white;
+`;
+
+export const LogoText = styled.h3`
+  font-family: "Kablammo";
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: normal;
+  /* font-variation-settings: "MORF" 0; */
+  font-size: 30px;
+  color: white;
+`;
