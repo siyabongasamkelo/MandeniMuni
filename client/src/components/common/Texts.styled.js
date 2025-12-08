2c372c662993ba229f75fb14853778a3c0fdaeb7
siyabonga <siyabongasamkelociam@gmail.com>
2025-12-09 00:50:15 +0200

    made text responsive to all sizes

--- client/src/components/common/Texts.styled.js
+++ client/src/components/common/Texts.styled.js
@@ -10,8 +10,26 @@ export const BigText = styled.h1`
   font-size: 3.8rem;
   color: white;
 
+  //Mobile
+
+  @media only screen and (min-width: 230px) {
+    text-align: center;
+    font-size: 3rem;
+  }
+
+  @media only screen and (min-width: 360px) {
+    font-size: 1.8rem;
+  }
+
+  @media only screen and (min-width: 414px) {
+    font-size: 3rem;
+  }
+
+  //Desktop
+
   @media only screen and (min-width: 1014px) {
     font-size: 1.8rem;
+    text-align: left;
   }
 
   @media only screen and (min-width: 1270px) {
@@ -42,8 +60,40 @@ export const SmallText = styled.p`
   font-size: 18px;
   color: white;
 
+  //Mobile
+  @media only screen and (min-width: 240px) {
+    text-align: center;
+    font-size: 1rem;
+  }
+
+  @media only screen and (min-width: 360px) {
+    font-size: 0.8rem;
+  }
+
+  @media only screen and (min-width: 380px) {
+    font-size: 0.93rem;
+  }
+
+  @media only screen and (min-width: 404px) {
+    font-size: 1rem;
+  }
+
+  @media only screen and (min-width: 480px) {
+    font-size: 1.2rem;
+  }
+
+  @media only screen and (min-width: 540px) {
+    font-size: 1.3rem;
+  }
+
+  @media only screen and (min-width: 600px) {
+    font-size: 1.4rem;
+  }
+
+  //Desktop
   @media only screen and (min-width: 1014px) {
     font-size: 13px;
+    text-align: left;
   }
 
   @media only screen and (min-width: 1270px) {
