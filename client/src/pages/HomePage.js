0105d096b94d7298f962514f1123c69c6a8467d8
siyabonga <siyabongasamkelociam@gmail.com>
2025-12-09 00:51:43 +0200

    displaying both mobile and home pages depending on device size

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,7 +1,13 @@
 import Home from "../components/HomePageComponents/Home";
+import MobileHome from "../components/HomePageComponents/MobileHome";
 
 const HomePage = () => {
-  return <Home />;
+  return (
+    <>
+      <Home />
+      <MobileHome />
+    </>
+  );
 };
 
 export default HomePage;
