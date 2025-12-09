79173d3465f39d9099bfdf01657f6d9c241ef883
siyabonga <siyabongasamkelociam@gmail.com>
2025-12-09 23:07:48 +0200

    added tablet mode to homepage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import Home from "../components/HomePageComponents/Home";
 import MobileHome from "../components/HomePageComponents/MobileHome";
+import TabletHome from "../components/HomePageComponents/TabletHome";
 
 const HomePage = () => {
   return (
     <>
       <Home />
       <MobileHome />
+      <TabletHome />
     </>
   );
 };
