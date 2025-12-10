36e6c014d7be994fa345ad0b4cfb63a22975da6c
siyabonga <siyabongasamkelociam@gmail.com>
2025-12-10 13:22:37 +0200

    styles for the mayor image file which consitst of 3 divs  displayed next to each other

---
+++ client/src/components/ImMandeniComponents/MayerText.js
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+import { SmallText } from "../common/Texts.styled";
+
+export const MayerContainers = styled.div`
+  height: 20%;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  .left {
+    width: 20%;
+    height: 100%;
+    .up {
+      height: 30%;
+      width: 20%;
+      margin-left: 40%;
+      background-color: #af47d2;
+    }
+    .down {
+      height: 30%;
+      width: 20%;
+      margin-left: 60%;
+      background-color: rgba(175, 71, 210, 0.5);
+    }
+  }
+  .center {
+    width: 30%;
+    height: 100%;
+    p {
+      text-align: center;
+      font-size: 1rem;
+      color: rgba(255, 255, 255, 0.8);
+    }
+  }
+  .right {
+    width: 20%;
+    height: 100%;
+  }
+`;
+
+const MayerText = () => {
+  return (
+    <MayerContainers>
+      <div className="left">
+        <div className="up"></div>
+        <div className="down"></div>
+      </div>
+      <div className="center">
+        <div>
+          <SmallText>
+            “2024 is a year of creating synergy in working relations with
+            various sector departments and delivering services that deals
+            directly with the plights of our people”. Mandeni Municipality has
+            committed 2024 to a robust community engagement and economic
+            development for our residents. It is a pleasure to once again share
+            with you the progress of our Municipality, as well as the future
+            projects that we wish to implement this year.
+          </SmallText>
+        </div>
+      </div>
+      <div className="right"></div>
+    </MayerContainers>
+  );
+};
+
+export default MayerText;
