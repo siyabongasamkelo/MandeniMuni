eee92bd9f79f893246970849214deb04235b12a6
siyabonga <siyabongasamkelociam@gmail.com>
2025-12-11 22:33:42 +0200

    lining category boxes for good display

--- client/src/components/ImMandeniComponents/CategoryBoxes.js
+++ client/src/components/ImMandeniComponents/CategoryBoxes.js
@@ -7,9 +7,114 @@ import secretary from "../../assets/img/secretary.png";
 import { SmallText } from "../common/Texts.styled";
 
 export const CategoryBoxesContainer = styled.div`
+  @media only screen and (min-width: 240px) {
+    height: 160vh;
+    width: 100%;
+    margin-top: 2%;
+
+    .cover {
+      height: 100%;
+      width: 90%;
+      margin-left: 2.5%;
+
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      .box {
+        height: 17.5%;
+        width: 80%;
+        margin-top: 8%;
+        margin-left: 5%;
+
+        background-color: #af47d2;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        img {
+          height: 50%;
+          aspect-ratio: 1 / 1;
+          margin-top: 15%;
+        }
+        p {
+          margin-top: 10%;
+        }
+      }
+    }
+  }
+
+  @media only screen and (min-width: 1014px) {
+    height: 15%;
+    width: 100%;
+    margin-top: 2%;
+
+    .cover {
+      height: 100%;
+      width: 55%;
+      margin-left: 22.5%;
+      margin-top: 0;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+
+      .box {
+        height: 80%;
+        width: 18%;
+        margin-top: 0%;
+        background-color: #af47d2;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        img {
+          height: 45%;
+          aspect-ratio: 1 / 1;
+          margin-top: 0;
+          margin-top: 10%;
+        }
+        p {
+          margin-top: 10%;
+        }
+      }
+    }
+  }
+
+  @media only screen and (min-width: 1680px) {
+    height: 20%;
+    width: 100%;
+    margin-top: 4%;
+
+    .cover {
+      height: 100%;
+      width: 55%;
+      margin-left: 22.5%;
+      display: block;
+      display: flex;
+      justify-content: space-between;
+
+      .box {
+        height: 80%;
+        width: 18%;
+        background-color: #af47d2;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        img {
+          height: 45%;
+          aspect-ratio: 1 / 1;
+          margin-top: 10%;
+        }
+        p {
+          margin-top: 10%;
+        }
+      }
+    }
+  }
+
   @media only screen and (min-width: 1910px) {
     height: 20%;
     width: 100%;
+    margin-top: 0;
 
     .cover {
       height: 100%;
