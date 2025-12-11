5c48a58e3a7771c792c397553be350f2b156c6dc
siyabonga <siyabongasamkelociam@gmail.com>
2025-12-11 22:41:30 +0200

    Mayor text content

--- client/src/components/ImMandeniComponents/MayerText.js
+++ client/src/components/ImMandeniComponents/MayerText.js
@@ -2,39 +2,154 @@ import styled from "styled-components";
 import { SmallText } from "../common/Texts.styled";
 
 export const MayerContainers = styled.div`
-  height: 20%;
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
+  @media only screen and (min-width: 1014px) {
+    height: 20%;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 2%;
 
-  .left {
-    width: 20%;
-    height: 100%;
-    .up {
-      height: 30%;
-      width: 20%;
-      margin-left: 40%;
-      background-color: #af47d2;
+    .left {
+      width: 27.5%;
+      height: 100%;
+      .up {
+        height: 30%;
+        width: 20%;
+        margin-left: 40%;
+        background-color: #af47d2;
+      }
+      .down {
+        height: 30%;
+        width: 20%;
+        margin-left: 60%;
+        background-color: rgba(175, 71, 210, 0.5);
+      }
     }
-    .down {
-      height: 30%;
-      width: 20%;
-      margin-left: 60%;
-      background-color: rgba(175, 71, 210, 0.5);
+    .center {
+      width: 45%;
+      height: 100%;
+      p {
+        text-align: center;
+        font-size: 0.8rem;
+        color: rgba(255, 255, 255, 0.8);
+      }
+    }
+    .right {
+      width: 27.5%;
+      height: 100%;
     }
   }
-  .center {
-    width: 30%;
-    height: 100%;
-    p {
-      text-align: center;
-      font-size: 1rem;
-      color: rgba(255, 255, 255, 0.8);
+
+  @media only screen and (min-width: 1270px) {
+    height: 20%;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 2%;
+
+    .left {
+      width: 27.5%;
+      height: 100%;
+      .up {
+        height: 30%;
+        width: 20%;
+        margin-left: 40%;
+        background-color: #af47d2;
+      }
+      .down {
+        height: 30%;
+        width: 20%;
+        margin-left: 60%;
+        background-color: rgba(175, 71, 210, 0.5);
+      }
+    }
+    .center {
+      width: 45%;
+      height: 100%;
+      p {
+        text-align: center;
+        font-size: 1rem;
+        color: rgba(255, 255, 255, 0.8);
+      }
+    }
+    .right {
+      width: 27.5%;
+      height: 100%;
     }
   }
-  .right {
-    width: 20%;
-    height: 100%;
+
+  @media only screen and (min-width: 1680px) {
+    height: 20%;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    .left {
+      width: 30%;
+      height: 100%;
+      .up {
+        height: 30%;
+        width: 20%;
+        margin-left: 40%;
+        background-color: #af47d2;
+      }
+      .down {
+        height: 30%;
+        width: 20%;
+        margin-left: 60%;
+        background-color: rgba(175, 71, 210, 0.5);
+      }
+    }
+    .center {
+      width: 40%;
+      height: 100%;
+      p {
+        text-align: center;
+        font-size: 1.2rem;
+        color: rgba(255, 255, 255, 0.8);
+      }
+    }
+    .right {
+      width: 30%;
+      height: 100%;
+    }
+  }
+
+  @media only screen and (min-width: 1910px) {
+    margin-top: 0;
+    height: 20%;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+
+    .left {
+      width: 20%;
+      height: 100%;
+      .up {
+        height: 30%;
+        width: 20%;
+        margin-left: 40%;
+        background-color: #af47d2;
+      }
+      .down {
+        height: 30%;
+        width: 20%;
+        margin-left: 60%;
+        background-color: rgba(175, 71, 210, 0.5);
+      }
+    }
+    .center {
+      width: 30%;
+      height: 100%;
+      p {
+        text-align: center;
+        font-size: 1rem;
+        color: rgba(255, 255, 255, 0.8);
+      }
+    }
+    .right {
+      width: 20%;
+      height: 100%;
+    }
   }
 `;
 
