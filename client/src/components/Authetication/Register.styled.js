158615edd390f29932555eacd85cbcacae1f1753
siyabonga <siyabongasamkelociam@gmail.com>
2025-12-16 11:56:47 +0200

    styles for register page for desktop

---
+++ client/src/components/Authetication/Register.styled.js
@@ -0,0 +1,119 @@
+import styled from "styled-components";
+
+export const RegisterContainer = styled.div`
+  overflow: hidden;
+`;
+
+export const RegisterCover = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: black;
+`;
+
+export const RegisterContent = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const FormContent = styled.div`
+  @media only screen and (min-width: 1910px) {
+    height: 100%;
+    width: 50%;
+    background-color: white;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .cover {
+      height: 80%;
+      width: 60%;
+      h3 {
+        color: black;
+        font-family: "League Spartan";
+        text-align: left;
+        font-size: 4rem;
+      }
+      p {
+        color: black;
+      }
+      .form {
+        height: 100%;
+        width: 100%;
+
+        input {
+          height: 55px;
+          border-radius: 10px;
+          width: 100%;
+          margin-top: 4%;
+          border: 1px solid rgba(0, 0, 0, 0.7);
+          padding-left: 20px;
+        }
+
+        .name {
+          width: 100%;
+          display: flex;
+          justify-content: space-between;
+          div {
+            width: 48%;
+            input {
+              width: 100%;
+            }
+          }
+        }
+
+        .terms {
+          margin-top: 5%;
+          display: flex;
+          .check {
+            width: 10%;
+            margin-left: -2%;
+          }
+          .text {
+            width: 90%;
+            font-size: 0.4rem;
+            margin-top: 1%;
+          }
+          input {
+            height: 30px;
+          }
+        }
+
+        button {
+          border-radius: 10px;
+          margin-top: 2%;
+        }
+
+        .divider {
+          display: flex;
+          align-items: center;
+          text-align: center;
+          color: #888;
+          margin: 20px 0;
+        }
+
+        .divider::before,
+        .divider::after {
+          content: "";
+          flex: 1;
+          border-bottom: 1px solid #e0e0e0;
+        }
+
+        .divider:not(:empty)::before {
+          margin-right: 10px;
+        }
+
+        .divider:not(:empty)::after {
+          margin-left: 10px;
+        }
+      }
+    }
+  }
+`;
+
+export const MandeniYethuInfoContent = styled.div`
+  height: 100%;
+  width: 50%;
+  background-color: white;
+`;
