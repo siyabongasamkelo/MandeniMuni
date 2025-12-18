b7b63d73941ec43212e22cf8feae1b5d701e6185
siyabonga <siyabongasamkelociam@gmail.com>
2025-12-18 17:27:34 +0200

    changed old home page moved all it components to a new folder called oldhome

--- client/src/components/HomePageComponents/Home.js
+++ client/src/components/HomePageComponents/Home.js
@@ -1,31 +1,84 @@
-import { DecorationsContainer, HomeContainer, HomeCover } from "./Home.styled";
-import BackgroundImage from "../../assets/img/background.jpeg";
 import Header from "../common/Header";
-import Div1 from "./Div1";
-import Div2 from "./Div2";
-import Div3 from "./Div3";
-import Div4 from "./Div4";
-import Div5 from "./Div5";
-import Div6 from "./Div6";
+import { HomeContainer, HomeContent, HomeCover } from "./Home.styled";
+import newphoto from "../../assets/img/new.png";
+import { BigText, MediumText, SmallText } from "../common/Texts.styled";
+import { Button } from "../common/Button.styled";
+import {
+  Facebook,
+  Instagram,
+  Twitter,
+  Whatsapp,
+  Youtube,
+} from "react-bootstrap-icons";
 
 const Home = () => {
   return (
     <HomeContainer>
-      <HomeCover image={BackgroundImage}>
-        <DecorationsContainer>
-          {/* because of the complexity of the interface i used position : absolute on the header */}
-          {/* to make it float above the home screen */}
+      <HomeCover>
+        <HomeContent>
           <Header />
-          {/* this whole interface is devided into 6 divs vertically */}
-          {/* each row contains it own components inside together they form this whole page */}
-
-          <Div1 />
-          <Div2 />
-          <Div3 />
-          <Div4 />
-          <Div5 />
-          <Div6 />
-        </DecorationsContainer>
+          <div className="content">
+            <div className="blur"></div>
+            <div className="image">
+              <img src={newphoto} alt="mandeni" />
+            </div>
+            <div className="big-text">
+              <BigText>Mandeni Yethu Youth Empowerment</BigText>
+            </div>
+            <div className="button">
+              <Button>Get Empowered</Button>
+            </div>
+            <div className="paragraph-members">
+              <div className="paragraph">
+                <div className="text">
+                  <SmallText>
+                    Mandeni is a community built on strength, resilience, and
+                    unity. Every day, our people rise with purpose — working
+                    hard, supporting one another, and shaping a future filled
+                    with opportunity. From our townships to our factories and
+                    farmlands, Mandeni stands as a place of courage, dignity,
+                    and hope.
+                  </SmallText>
+                </div>
+                <div className="socialmedia-icons">
+                  <Facebook />
+                  <Whatsapp />
+                  <Twitter />
+                  <Youtube />
+                  <Instagram />
+                </div>
+              </div>
+              <div className="members">
+                <div class="avatars">
+                  <img
+                    class="avatar"
+                    src="https://i.pravatar.cc/100?img=1"
+                    alt="User"
+                  />
+                  <img
+                    class="avatar"
+                    src="https://i.pravatar.cc/100?img=2"
+                    alt="User"
+                  />
+                  <img
+                    class="avatar"
+                    src="https://i.pravatar.cc/100?img=3"
+                    alt="User"
+                  />
+                  <img
+                    class="avatar"
+                    src="https://i.pravatar.cc/100?img=4"
+                    alt="User"
+                  />
+                  <div class="avatar more">+99</div>
+                </div>
+                <MediumText>
+                  Join The <br></br>Community
+                </MediumText>
+              </div>
+            </div>
+          </div>
+        </HomeContent>
       </HomeCover>
     </HomeContainer>
   );
--- client/src/components/HomePageComponents/JoinNow.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { JoinNowContainer } from "./JoinNow.styled";
-
-const JoinNow = ({ pic }) => {
-  return <JoinNowContainer pic={pic}></JoinNowContainer>;
-};
-
-export default JoinNow;
