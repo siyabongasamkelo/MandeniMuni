795e07c5a28bc9a691950aef5168bd748cecf36c
siyabonga <siyabongasamkelociam@gmail.com>
2025-12-19 12:04:23 +0200

    made button responsive on home page for different desktop sizes

--- client/src/components/common/Button.styled.js
+++ client/src/components/common/Button.styled.js
@@ -1,10 +1,37 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
-  height: 80px;
-  width: 100%;
-  border-radius: 40px;
-  border: none;
-  background-color: ${(props) => props.theme.color.primary};
-  color: white;
+  @media only screen and (min-width: 1014px) {
+    height: 50px;
+    width: 100%;
+    border-radius: 40px;
+    border: none;
+    background-color: ${(props) => props.theme.color.primary};
+    color: white;
+  }
+
+  @media only screen and (min-width: 1270px) {
+    height: 60px;
+    width: 100%;
+  }
+
+  @media only screen and (min-width: 1430px) {
+    height: 70px;
+    width: 100%;
+  }
+
+  @media only screen and (min-width: 1910px) {
+    height: 80px;
+    width: 100%;
+  }
+
+  @media only screen and (min-width: 3840px) {
+    height: 100px;
+    width: 70%;
+  }
+
+  @media only screen and (min-width: 3840px) {
+    height: 100px;
+    width: 40%;
+  }
 `;
